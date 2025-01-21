3bf16544e9ca7e1763e44e04533821324b89ed37
kanhaiya Kumar <97240685+kanhaiyakumar1838@users.noreply.github.com>
2025-01-21 11:57:21 +0530

    Update QuizPage.jsx

--- client/src/components/QuizPage.jsx
+++ client/src/components/QuizPage.jsx
@@ -15,7 +15,7 @@ const QuizPage = () => {
 
   useEffect(() => {
     axios
-  .get("https://ultimate-quiz-back.vercel.app/api/questions")
+  .get("ultimate-quiz-back.vercel.app/api/questions")
   .then((response) => {
     setQuestions(response.data);
     localStorage.setItem("quizQuestions", JSON.stringify(response.data));
